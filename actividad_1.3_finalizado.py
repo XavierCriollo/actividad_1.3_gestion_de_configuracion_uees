c3dfd4c319e4ded0221c526eb3d5f1db48cb9afb
luis_criollo_uees <luisxavier_23@hotmail.com>
2026-02-27 10:42:51 -0500

    Rama B:Segundo commit se comprueba el funcionamiento de la funcion para el mensaje

--- actividad_1.3_finalizado.py
+++ actividad_1.3_finalizado.py
@@ -3,7 +3,7 @@ print("Los saluda los alumnos de la UEES")
 
 # rama B
 def mostrar_mensaje():
-    print('funcionalidad_b_version_1')
+    print('comprobacion_funcionabilidad_b_version_2')
 
 if __name__ == '__main__':
     mostrar_mensaje()
