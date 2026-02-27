8c17783ac8ab573f5fd41029dc8be6be17dae370
luis_criollo_uees <luisxavier_23@hotmail.com>
2026-02-27 11:07:35 -0500

    revision final del main, borrar duplicados, funcionamiento correcto de la funcion

--- actividad_1.3_finalizado.py
+++ actividad_1.3_finalizado.py
@@ -1,11 +1,6 @@
 print("Hola mundo")
 print("Los saluda los alumnos de la UEES")
 
-def mostrar_mensaje():
-    print('comprobacion_funcionamiento_version_2')
-
-if __name__ == '__main__':
-    mostrar_mensaje()
 #-----------------------------------------------------
 # Esta es la segunda pratica en control de versiones con GIT 
 
