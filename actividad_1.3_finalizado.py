929e43abd39bac8f8fd2cfb2958ed9c92b34beb3
luis_criollo_uees <luisxavier_23@hotmail.com>
2026-02-27 10:25:19 -0500

    Para este primer ejecicio nodificamos la rama main con la finalidad de probar en las otras ramas

--- actividad_1.3_finalizado.py
+++ actividad_1.3_finalizado.py
@@ -9,3 +9,8 @@ print(a + b)
 print(a - b)
 
 print("Proyecto finalizado")
+
+#-----------------------------------------------------
+# Esta es la segunda pratica en control de versiones con GIT 
+
+print("Primer ejercicio en el main")
